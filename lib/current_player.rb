babd64f4df6ecfa827de5427f5f5d2dc1d8b2b61
Meg Galvez <meg@oakmaine.com>
2019-03-11 16:22:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,14 +10,3 @@ def turn_count(board)
   return turns 
 end
 
-def turn_count(array)
-  counter = 0
-  array.each do |element|
-    if (element == "X" || element == "O")
-      counter += 1
-    else
-      # do nothing
-    end
-  end
-  return counter
-end
