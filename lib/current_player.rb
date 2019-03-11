302073906e7df0f9ab8260e72784e42a2b873018
Meg Galvez <meg@oakmaine.com>
2019-03-11 16:21:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0 
   board.each do |position|
-    if position == "X" || position == "O"
+    if (position == "X" || position == "O")
       turns += 1 
     end 
     turns 
